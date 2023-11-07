ab42698f1b851d6fb12468c9b3656ed500d9edc5
Niraj Shah <nirajshah950@gmail.com>
2023-11-07 22:25:08 +0530

    updated change3

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const http = require("http")
 const mongoose = require("mongoose")
 const app = require("./app")
+const dotenv = require("dotenv")
+
+dotenv.config({
+    path:"./.env"
+})
 
 
 // Connect to DB
-mongoose.connect("mongodb://127.0.0.1:27017/ToDoTasks").then(() => {
+// mongoose.connect("mongodb://127.0.0.1:27017/ToDoTasks").then(() => {
+//     console.log("Connected to DB")
+// }).catch((err) => {
+//     console.log(err)
+// })
+
+// Connect to db atlas
+// local
+// cloud 
+mongoose.connect(process.env.MONGODB_CLOUD).then(() => {
     console.log("Connected to DB")
 }).catch((err) => {
     console.log(err)
@@ -12,8 +26,8 @@ mongoose.connect("mongodb://127.0.0.1:27017/ToDoTasks").then(() => {
  
 
 let server = http.createServer(app)
-let PORT = 5000
-server.listen(PORT, (err) => {
+
+server.listen(process.env.PORT, (err) => {
     if (err) console.log(err)
-    console.log("SERVER RUNNING AT PORT NO : " + PORT)
+    console.log("SERVER RUNNING AT PORT NO : " + process.env.PORT)
 })
